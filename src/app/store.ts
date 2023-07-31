d2bfd1d0ecb7f33af08cb0966d744a72c5291232
zenghyun <pipo36@naver.com>
2023-07-31 16:35:57 +0900

    disunite types

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postsReducer, { PostListsType } from "../features/posts/postsSlice";
-import usersReducer, { UserListType } from "../features/users/usersSlice";
+import postsReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/usersSlice";
 import { combineReducers } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
 import sessionStorage from "redux-persist/es/storage/session";
-import notificationReducer from '../features/notifications/notificationsSlice';
+import notificationReducer from "../features/notifications/notificationsSlice";
+import { PostListsType } from "../type/postType";
+import { UserListType } from "../type/userType";
 import { NotificationListType } from "../type/NotificationsType";
 import { useDispatch } from "react-redux";
 
@@ -21,7 +23,7 @@ const reducers = combineReducers({
 });
 
 const persistConfig = {
-  key: 'root', // sessionStorage key 
+  key: "root", // sessionStorage key
   storage: sessionStorage, // sessionStorage
 };
 
@@ -29,11 +31,12 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch = () => useDispatch<AppDispatch>(); 
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export default store;
