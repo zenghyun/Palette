c3779e03e3b0f6d5ebb1813f7e4ecd432cbde9e2
zenghyun <pipo36@naver.com>
2023-07-31 16:40:12 +0900

    change type location

--- src/components/common/PostAuthor.tsx
+++ src/components/common/PostAuthor.tsx
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
-import { UserListType, UserStateType } from "../../features/users/usersSlice";
+import { UserListType, UserStateType } from "../../type/userType";
 import { selectUserById } from "../../features/users/usersSlice";
 
 export const PostAuthor = ({ userId }: { userId: string }) => {
   const author = useSelector((state: UserListType) =>
-  selectUserById(state, userId)
-) as UserStateType;
+    selectUserById(state, userId)
+  ) as UserStateType;
 
   return <span>작성자 : {author ? author.name : "Unknown author"}</span>;
 };
--- src/components/post/AddPostForm.tsx
+++ src/components/post/AddPostForm.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from "react";
 import { useSelector } from "react-redux";
-import { UserStateType } from "../../features/users/usersSlice";
+import { UserStateType } from "../../type/userType";
 // import { addNewPost, InitialPostType } from "../../features/posts/postsSlice";
 import { postAdded } from "../../features/posts/postsSlice";
 import { useAppDispatch } from "../../app/store";
@@ -8,7 +8,7 @@ import { selectAllUsers } from "../../features/users/usersSlice";
 import { useNavigate } from "react-router-dom";
 import { fetchNotifications } from "../../features/notifications/notificationsSlice";
 
- const AddPostForm = () => {
+const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
@@ -23,27 +23,28 @@ import { fetchNotifications } from "../../features/notifications/notificationsSl
     setTitle(e.target.value);
   const onContentChanged = (e: ChangeEvent<HTMLTextAreaElement>) =>
     setContent(e.target.value);
-  const onAuthorChanged = (e: ChangeEvent<HTMLSelectElement>) => setUserId(e.target.value);
+  const onAuthorChanged = (e: ChangeEvent<HTMLSelectElement>) =>
+    setUserId(e.target.value);
 
   const onSavePostClicked = () => {
     if (title && content) {
       dispatch(postAdded(title, content, userId));
-      dispatch(fetchNotifications())
+      dispatch(fetchNotifications());
     }
     setTitle("");
     setContent("");
-    navigate('/');
+    navigate("/");
   };
-  
+
   const canSave = [title, content, userId].every(Boolean);
 
-  // 실제 Backend API에 data 추가 가능할 시 logic 
+  // 실제 Backend API에 data 추가 가능할 시 logic
 
   // const canSave =
   //   [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
 
   // const initialPost = { title, content, user: userId} as InitialPostType;
-  
+
   // const onSavePostClicked = async () => {
   //   if (canSave) {
   //     try {
@@ -60,9 +61,9 @@ import { fetchNotifications } from "../../features/notifications/notificationsSl
   //   }
   // }
 
-  const usersOptions = users.map((user : UserStateType) => (
+  const usersOptions = users.map((user: UserStateType) => (
     <option key={user.id} value={user.id}>
-        {user.name}
+      {user.name}
     </option>
   ));
 
@@ -78,11 +79,11 @@ import { fetchNotifications } from "../../features/notifications/notificationsSl
           value={title}
           onChange={onTitleChanged}
         />
-           <label htmlFor="postAuthor">작성자</label>
-           <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
-            <option value=""></option>
-            {usersOptions}
-           </select>
+        <label htmlFor="postAuthor">작성자</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">내용</label>
         <textarea
           id="postContent"
