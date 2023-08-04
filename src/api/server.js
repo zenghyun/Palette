3fdbc018d323b493b55d1b629868ae1ed41ec064
zenghyun <pipo36@naver.com>
2023-08-05 01:35:25 +0900

    change NUM_USERS

--- src/api/server.js
+++ src/api/server.js
@@ -10,7 +10,7 @@ import { parseISO } from "date-fns";
 
 faker.locale = "ko";
 
-const NUM_USERS = 5;
+const NUM_USERS = 20;
 // const POSTS_PER_USER = line 194
 const RECENT_NOTIFICATIONS_DAYS = 7;
 
