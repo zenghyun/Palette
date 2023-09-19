765c80385f39c76fbb98df84e43b10c72b0cf13b
zenghyun <pipo36@naver.com>
2023-09-19 16:08:04 +0900

    set empty NotificationList text

--- src/components/notifications/NotificationsList.tsx
+++ src/components/notifications/NotificationsList.tsx
@@ -15,6 +15,11 @@ const NotificationsListBlock = styled.section`
     overflow: hidden auto;
     height: 700px;
   }
+
+  .emptyNotifiList {
+    color: red;
+  }
+
   .notification.new {
     background-color: rgba(29, 161, 242, 0.1);
   }
@@ -29,8 +34,23 @@ const NotificationsListBlock = styled.section`
 const NotificationsList = ({
   renderedNotifications,
 }: {
-  renderedNotifications: JSX.Element[];
+  renderedNotifications: JSX.Element[] | null;
 }) => {
+  if(renderedNotifications === null || renderedNotifications.length === 0) {
+    return (
+      <NotificationsListBlock>
+      <h2>Notifications</h2>
+      <div className="notifiList">
+        <span className="emptyNotifiList">
+            알림이 존재하지 않습니다.  <br/>
+            게시글의 reaction button을 누르거나, <br/>
+            새로운 게시글을 작성하면 알림을 받을 수 있습니다.
+        </span>
+        </div>
+    </NotificationsListBlock>
+    )
+  }
+
   return (
     <NotificationsListBlock>
       <h2>Notifications</h2>
